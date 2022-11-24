e0b5a92875246f540512a1c0b7ef6874f20adb43
Patrick Eddy <p@patrickeddy.com>
2022-11-23 19:25:06 -1000

    Add authorization check to funcs that require it

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -2,14 +2,14 @@ import fetchMock from "fetch-mock";
 import apiClient from "./api-client";
 import TheOneClient from "./index";
 
+let client: TheOneClient;
+
+beforeEach(() => {
+  client = new TheOneClient({ accessToken: "mock-token" });
+  fetchMock.restore();
+});
+
 describe("Book", () => {
-  let client: TheOneClient;
-
-  beforeEach(() => {
-    client = new TheOneClient({ accessToken: "" });
-    fetchMock.restore();
-  });
-
   describe("LIST books", () => {
     it("success", async () => {
       fetchMock.getOnce(`${apiClient.baseUrl}/book`, {
@@ -52,20 +52,33 @@ describe("Book", () => {
 
   describe("LIST book chapters", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/book/1/chapter`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }, { _id: "2" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/book/1/chapter`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }, { _id: "2" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.bookChapters("1")).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }, { _id: "2" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/book/1/chapter`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/book/1/chapter`,
+        {
+          status: 500,
+          headers: { authorization: "Bearer mock-token" },
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.bookChapters("1")).rejects.toThrow(
         "server error"
       );
@@ -74,117 +87,151 @@ describe("Book", () => {
 });
 
 describe("Movie", () => {
-  let client: TheOneClient;
-
-  beforeEach(() => {
-    client = new TheOneClient({ accessToken: "" });
-    fetchMock.restore();
-  });
-
   describe("LIST movies", () => {
     it("LIST movies", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/movie`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }, { _id: "2" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/movie`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }, { _id: "2" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.movies()).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }, { _id: "2" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/movie`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/movie`,
+        {
+          status: 500,
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.movies()).rejects.toThrow("server error");
     });
   });
 
   describe("GET a movie", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/movie/1`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/movie/1`,
+        {
+          status: 200,
+          headers: { authorization: "Bearer mock-token" },
+          body: { docs: [{ _id: "1" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.movie("1")).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/movie/1`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/movie/1`,
+        {
+          status: 500,
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.movie("1")).rejects.toThrow("server error");
     });
   });
 });
 
 describe("Quote", () => {
-  let client: TheOneClient;
-
-  beforeEach(() => {
-    client = new TheOneClient({ accessToken: "" });
-    fetchMock.restore();
-  });
-
   describe("LIST quotes", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/quote`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }, { _id: "2" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/quote`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }, { _id: "2" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.quotes()).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }, { _id: "2" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/quote`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/quote`,
+        {
+          status: 500,
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.quotes()).rejects.toThrow("server error");
     });
   });
 
   describe("GET a quote", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/quote/1`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/quote/1`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.quote("1")).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/quote/1`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/quote/1`,
+        {
+          status: 500,
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.quote("1")).rejects.toThrow("server error");
     });
   });
 });
 
 describe("Character", () => {
-  let client: TheOneClient;
-
-  beforeEach(() => {
-    client = new TheOneClient({ accessToken: "" });
-    fetchMock.restore();
-  });
-
   describe("LIST characters", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/character`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }, { _id: "2" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/character`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }, { _id: "2" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.characters()).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }, { _id: "2" }] })
       );
@@ -201,20 +248,32 @@ describe("Character", () => {
 
   describe("GET a character", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/character/1`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/character/1`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.character("1")).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/character/1`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/character/1`,
+        {
+          status: 500,
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.character("1")).rejects.toThrow(
         "server error"
       );
@@ -223,49 +282,66 @@ describe("Character", () => {
 });
 
 describe("Chapter", () => {
-  let client: TheOneClient;
-
-  beforeEach(() => {
-    client = new TheOneClient({ accessToken: "" });
-    fetchMock.restore();
-  });
-
   describe("LIST chapters", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/chapter`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }, { _id: "2" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/chapter`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }, { _id: "2" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.chapters()).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }, { _id: "2" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/chapter`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/chapter`,
+        {
+          status: 500,
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.chapters()).rejects.toThrow("server error");
     });
   });
 
   describe("GET a chapter", () => {
     it("success", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/chapter/1`, {
-        status: 200,
-        body: { docs: [{ _id: "1" }] },
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/chapter/1`,
+        {
+          status: 200,
+          body: { docs: [{ _id: "1" }] },
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       expect(await client.chapter("1")).toEqual(
         expect.objectContaining({ data: [{ _id: "1" }] })
       );
     });
 
     it("error", async () => {
-      fetchMock.getOnce(`${apiClient.baseUrl}/chapter/1`, {
-        status: 500,
-        body: "server error",
-      });
+      fetchMock.getOnce(
+        `${apiClient.baseUrl}/chapter/1`,
+        {
+          status: 500,
+          body: "server error",
+        },
+        {
+          headers: { authorization: "Bearer mock-token" },
+        }
+      );
       return expect(() => client.chapter("1")).rejects.toThrow("server error");
     });
   });
